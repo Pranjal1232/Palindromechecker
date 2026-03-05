aee23c45887d479e08ff56544017c14b98e5f07e
Pranjal1232 <pg3729@srmist.edu.in>
2026-03-05 10:01:44 +0530

    palindrome app

--- APP/src/PalindromeCheckerApp.java
+++ APP/src/PalindromeCheckerApp.java
@@ -1,7 +1,25 @@
 public class PalindromeCheckerApp {
     static void main(String[] args) {
-        System.out.println("Welcome to the Palindrome checker data management");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialised successfully");
+        // Hardcoded string
+        String input = "madam";
+
+        boolean isPalindrome = true;
+
+        // Loop only till half of the string length
+        for (int i = 0; i < input.length() / 2; i++) {
+
+            if (input.charAt(i) != input.charAt(input.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Print result
+        if (isPalindrome) {
+            System.out.println(input + " is a Palindrome.");
+        } else {
+            System.out.println(input + " is NOT a Palindrome.");
+        }
+
     }
 }
